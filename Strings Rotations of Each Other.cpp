cfac53737a4426e2f0951088e34e1233a9010e24
Jaidev B <jaidevsboa@gmail.com>
2025-01-16 21:03:05 +0530

    Create Strings Rotations of Each Other.cpp
    Day 20

---
+++ Strings Rotations of Each Other.cpp
@@ -0,0 +1,10 @@
+
+class Solution {
+  public:
+    // Function to check if two strings are rotations of each other or not.
+    bool areRotations(string &s1, string &s2) {
+        if(s1.length() != s2.length()) return false;
+        string concat = s1 + s1;
+        return concat.find(s2) != string::npos;
+    }
+};
