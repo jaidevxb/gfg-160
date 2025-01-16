b33f5d9eba0f4595a5255606bcfb945d9780bd48
Jaidev B <jaidevsboa@gmail.com>
2025-01-16 20:47:32 +0530

    Create Second_Largest.cpp
    Array Day 1

---
+++ Second_Largest.cpp
@@ -0,0 +1,20 @@
+// User function template for C++
+class Solution {
+  public:
+    // Function returns the second
+    // largest elements
+    int getSecondLargest(vector<int> &arr) {
+        int largest = arr[0];
+        int slargest = -1;
+        for(int i = 1; i<arr.size(); i++){
+            if(arr[i] > largest){
+                slargest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i] < largest && arr[i] > slargest){
+                slargest = arr[i];
+            }
+        }
+        return slargest;
+    }
+};
