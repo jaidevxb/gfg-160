1b67257cd5f84392b74a45835edf1512c1c65c22
Jaidev B <jaidevsboa@gmail.com>
2025-01-16 20:52:02 +0530

    Create Next_Permutation.cpp
    Day 5

---
+++ Next_Permutation.cpp
@@ -0,0 +1,9 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    void nextPermutation(vector<int>& arr) {
+        // code here
+        next_permutation(arr.begin(), arr.end());
+    }
+};
